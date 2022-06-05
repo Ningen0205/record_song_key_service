b582ecc088c54d4d78446993618cd15b07944e7d
ningen <msg1801022@gmail.com>
2022-06-05 20:13:06 +0900

    add db

---
+++ src/driver/db.go
@@ -0,0 +1,54 @@
+package driver
+
+import (
+	"fmt"
+	"log"
+	"record_song_key_service/src/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DatabaseDriver struct {
+	db *gorm.DB
+}
+
+var dbInstance *DatabaseDriver
+
+func GetInstance() *DatabaseDriver {
+	if dbInstance == nil {
+		dbInstance = &DatabaseDriver{}
+	}
+
+	return dbInstance
+}
+
+func (this DatabaseDriver) GetConnection(host, user, password, dbname, port string) (db *gorm.DB) {
+	if this.db == nil {
+
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, dbname, port)
+		var err error
+		this.db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Panicln(err)
+			panic("failed to connect database")
+		}
+
+		err = this.db.AutoMigrate(
+			&model.Key{},
+			&model.Note{},
+			&model.Singer{},
+			&model.Song{},
+			&model.CoverSongKey{},
+			&model.User{},
+			&model.UserSong{},
+		)
+
+		if err != nil {
+			log.Panicln(err)
+			panic("failed to migrate database")
+		}
+	}
+
+	return this.db
+}
--- src/server.go
+++ src/server.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"record_song_key_service/src/driver"
+
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func loadEnv() {
+	err := godotenv.Load("../.env")
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+var dbDriver *gorm.DB
+
+func initialize() {
+	loadEnv()
+
+	dbDriver := driver.GetInstance()
+
+	log.Printf(os.Getenv("POSTGRES_HOST"))
+	dbDriver.GetConnection(
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_OUT_PORT"),
+	)
+}
+
 func main() {
+	initialize()
+
 	engine := gin.Default()
 
 	engine.GET("/", func(c *gin.Context) {
